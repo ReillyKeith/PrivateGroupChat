170e0354ce47d581d2684d1492de941806942e62
Keith Reilly <keithreilly@pm.me>
2020-02-05 17:38:43 +0000

    Got the chat working, at least storing the info in the array now
    Did some clean up with the chat as I had messed up with the users.js file. Fixed that now. So I can see in the console output that I am storing the messages

--- Group-Chat/server/users.js
+++ Group-Chat/server/users.js
@@ -1,36 +1,29 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-    name = name.trim().toLowerCase(); 
-    room = room.trim().toLowerCase(); 
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    const existingUser = users.find((user) => user.room === room && user.name === name);
+  const existingUser = users.find((user) => user.room === room && user.name === name);
 
-    if (existingUser){
-        return{ error: 'Username is taken'};
-    }
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
 
-    const user = { id, name, room};
-    users.push(user);
+  const user = { id, name, room };
 
-    return { user }
+  users.push(user);
 
+  return { user };
 }
 
-const removeUser =(id) => {
-    const index = users.findIndex((user) => user.id === id);
-    
-    if(index !== -1){
-        return users.splice(index, 1)[0];
-    }
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
 
+  if(index !== -1) return users.splice(index, 1)[0];
 }
 
-
-
-const getUser =() => (id) => users.find((users) => user.id === id);
-
+const getUser = (id) => users.find((user) => user.id === id);
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = { addUser, removeUser, getUser, getUsersInRoom};
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
