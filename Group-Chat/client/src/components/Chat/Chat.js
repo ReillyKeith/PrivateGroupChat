ad81d349f3aa06507d08c6f9b2ef29d8180d695a
Keith Reilly <keithreilly@pm.me>
2020-04-11 19:19:49 +0100

    Fixed connection error
    Talking to server, was pointed at wrong address

--- Group-Chat/client/src/components/Chat/Chat.js
+++ Group-Chat/client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+  const ENDPOINT = `localhost:5000`;
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
