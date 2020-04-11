0fb2311382032e43998274345e300660968443a2
Keith Reilly <keithreilly@pm.me>
2020-04-11 19:23:52 +0100

    Update Chat.js
    Set endpoint to http://1234.ie

--- Group-Chat/client/src/components/Chat/Chat.js
+++ Group-Chat/client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = `localhost:5000`;
+  const ENDPOINT = `http://1234.ie:5000`;
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
