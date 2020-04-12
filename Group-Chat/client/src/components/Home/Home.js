49706b7bebad4bd6c0ad1cb3e4cf7961c964aa91
Keith Reilly <keithreilly@pm.me>
2020-04-12 16:53:42 +0100

    Adding in links
    Need to add ICONs for each, but adding in links to home page

--- Group-Chat/client/src/components/Home/Home.js
+++ Group-Chat/client/src/components/Home/Home.js
@@ -12,7 +12,11 @@ const Home = () => {
                 <p className="text" >Join a room and start talking...</p>
                 <Link to={`/Join`}>
                     <button className="button mt-20" type="submit">Room Setup</button>
-                </Link>    
+                </Link>  
+                <a class="fa fa-facebook-square fa-4x" href="https://www.facebook.com/sharer/sharer.php?u=http://www.1234.ie"/>
+                <a class="tw tw-twitter-square tw-4x" href="https://twitter.com/home?status=http://www.1234.ie ">Tweet</a>
+                <a class="pn pn-pinterest-square pn-4x" href="https://pinterest.com/pin/create/button/?url=http://www.1234.ie&media=&description="/>
+                <a class="pn pn-pinterest-square pn-4x" href="https://www.linkedin.com/shareArticle?mini=true&url=http://www.1234.ie&title=&summary=&source="/>
             </div>
         </div>
     )
