034e32ffce09babf6af6c4d9d70e1c980b6c6cac
Keith Reilly <keithreilly@pm.me>
2020-04-17 21:25:35 +0100

    Update Home.js
    remove social links
    no point

--- Group-Chat/client/src/components/Home/Home.js
+++ Group-Chat/client/src/components/Home/Home.js
@@ -43,10 +43,6 @@ const Home = () => {
                     <button className="button mt-20" type="submit">Room Setup</button>
                 </Link>  
 
-                <a class="fa-facebook-square" href="https://www.facebook.com/sharer/sharer.php?u=http://www.1234.ie"/>
-                <a class="tw tw-twitter-square" href="https://twitter.com/home?status=http://www.1234.ie "/>
-                <a class="pn-pinterest-square" href="https://pinterest.com/pin/create/button/?url=http://www.1234.ie&media=&description="/>
-                <a class="lk-linkedin-square" href="https://www.linkedin.com/shareArticle?mini=true&url=http://www.1234.ie&title=&summary=&source="/>
             </div>
         </div>
     )
