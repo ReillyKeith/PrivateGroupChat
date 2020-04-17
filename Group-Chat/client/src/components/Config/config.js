789fb17e68839e042bfb6ca587fd2bf3c99e8ee2
Keith Reilly <keithreilly@pm.me>
2020-04-17 22:18:21 +0100

    Update config.js
    missed a bit

--- Group-Chat/client/src/components/Config/config.js
+++ Group-Chat/client/src/components/Config/config.js
@@ -5,5 +5,5 @@ at the moment it will be used to setup the
 endpoint server that the client talks to
 */
 
-const ENDPOINT = `https://1234.ie:5000`;
+const ENDPOINT = `http://www.1234.ie:5000`;
 module.exports = { ENDPOINT };
