639572e39f5ae08a5c7c59de7e2b315ae60cb813
Keith Reilly <keithreilly@pm.me>
2020-04-17 22:26:04 +0100

    Update config.js
    LOCALHOST

--- Group-Chat/client/src/components/Config/config.js
+++ Group-Chat/client/src/components/Config/config.js
@@ -5,5 +5,5 @@ at the moment it will be used to setup the
 endpoint server that the client talks to
 */
 
-const ENDPOINT = `localhost:5000`;
+const ENDPOINT = `127.0.0.1:5000`;
 module.exports = { ENDPOINT };
