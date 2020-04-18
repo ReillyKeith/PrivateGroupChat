d88590f31b2651fcaa4cbbc00921a903de88ce95
Keith Reilly <keithreilly@pm.me>
2020-04-18 13:25:38 +0100

    back to default
    defatult

--- Group-Chat/server/index.js
+++ Group-Chat/server/index.js
@@ -1,18 +1,11 @@
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
-const options = {
-  key: fs.readFileSync('/home/ec2-user/key.pem'),
-  cert: fs.readFileSync('/home/ec2-user/cert.pem')
-};
-
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const app = express();
-const server = https.createServer(options,app);
+const server = http.createServer(app);
 const io = socketio(server);
 
 // Count users active
